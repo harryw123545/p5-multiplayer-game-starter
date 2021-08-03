0c94e333c55ab94f17f05be7b140ed7ba1bef23a
harryw123545 <harrywakeling@hotmail.co.uk>
2021-08-03 14:32:33 +0100

    added superformula

--- public/display.js
+++ public/display.js
@@ -1,99 +1,73 @@
 const socket = io.connect('http://localhost');
-//let button1, button2;
-//let val1 = 0;
-//let val2 = 0;
-let val, val2;
+let val1 = 0;
+let val2 = 0;
+var n1 = 0.3;
+var n2 = 0.3;
+var n3 = 0.3;
+var m = 6;
+var a = 1;
+var b = 1;
+
+var osc = 0;
 
 let players = [];
-socket.on("blob", (blob)=>{
-          //console.log(blob);
-    console.log(blob.totalButton1);
-    console.log(blob.totalButton2);
-    val = blob.totalButton1;
-    val2 = blob.totalButton2;
-          });
+
+
 
 function setup() {
   createCanvas(400, 400);
+    socket.on("blob", (blob)=>{
+    console.log(blob.totalButton1);
+    console.log(blob.totalButton2);
+    val1 = blob.totalButton1*10;
+    val2 = blob.totalButton2*10;
+          });
+}
+
+function superShape(theta) { 
     
-//  button1 = createButton('option 1');
-//  button2 = createButton('option 2'); 
-//    
-//  button1.position(0, 0);
-//  button2.position(width/2, 0);
-//    
-//  button1.mousePressed(buttonCount1);
-//  button2.mousePressed(buttonCount2);
+    var part1 = (1 / a) * cos(theta * m/4);
+    part1 = abs(part1);
+    part1 = pow(part1, n2);
     
-  //socket.on('button', newButton);
-  //socket.on('button2', newButton2);
+    var part2 = (1 / b) * sin(theta * m/4);
+    part2 = abs(part2);
+    part2 = pow(part2, n3);
+    
+    var part3 = pow(part1 + part2, 1/n1);
+    
+    if (part3 === 0){
+        return 0;
+    }
+    return 1 / part3;
+    
 }
 
 function draw() {
-  //background(220);
-  //players.forEach(player => player.draw());
-    circle(100,100,val%100);
-    circle(200,200,val2%100);
-}
-
-
-//function newButton(buttonData) {
-//    fill(255, 100, 100);
-//    circle(0, height/2, buttonData % 200);
-//    console.log("received: ", buttonData);
-//
-//}
-//
-//function newButton2(buttonData2) {
-//    fill(255, 100, 100);
-//    circle(width/2, height/2, buttonData2 % 200);
-//    console.log("received: ", buttonData2);
-//
-//}
-
-//function buttonCount1() {
-// 
-//  //var buttonData = val1;
-//    
-//  //val1++;
-//  
-//  //console.log("sent val1: ", buttonData);
-//  socket.emit('button', 1);
-//}
-//
-//function buttonCount2() {
-// 
-//  //var buttonData2 = val2;
-//    
-//  //val2++;
-//  
-//  //console.log("sent val2: ", buttonData2);
-//  socket.emit('button', 2);
-//}
-
-
-function updatePlayers(serverPlayers) {
-  let removedPlayers = players.filter(p => serverPlayers.findIndex(s => s.id == p.id));
-  for (let player of removedPlayers) {
-    removePlayer(player.id);
-  }
-  for (let i = 0; i < serverPlayers.length; i++) {
-    let playerFromServer = serverPlayers[i];
-    if (!playerExists(playerFromServer)) {
-      players.push(new Player(playerFromServer));
+  m = map(sin(osc), -1, 1, 0, 10);
+    
+  osc += 0.01;
+    
+  background(51);
+  translate(width/2, height/2);
+    
+  console.log(val1, val2);    
+  stroke(255);
+  fill(val1 % 255, val2 % 255, 100);
+    
+  var radius = 300;
+    
+  var total = 500;
+  var increment = TWO_PI / total;
+    
+  beginShape();
+    for(var angle = 0; angle < TWO_PI; angle += increment){
+        var r = superShape(angle);
+        var x = radius * r * cos(angle);
+        var y = radius * r * sin(angle);
+        
+        vertex(x, y);
     }
-  }
+  endShape(CLOSE);
 }
 
-function playerExists(playerFromServer) {
-  for (let i = 0; i < players.length; i++) {
-    if (players[i].id === playerFromServer) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function removePlayer(playerId) {
-  players = players.filter(player => player.id !== playerId);
-}
